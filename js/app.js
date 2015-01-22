32dc48802663af4751bf21f74d8ffbfdb9d42039
jpbatz <jph@hawaii.edu>
2015-01-22 02:44:13 -1000

    MBA implements step 12

--- js/app.js
+++ js/app.js
@@ -7,39 +7,46 @@ $(document).ready(function() {
     if(input !== "") {
       listUserRepos(input);
     } else {
-      listDefaultRepo();
+      listDevLeagueRepos();
+      listGeemailCommits();
+      listTheRemixCommits();
     }
 
   });
 
   var listUserRepos = function(username) {
-    // console.log(username);
     $('.result').append("<h2>"+username+"</h2>");
     $.get( "https://api.github.com/users/"+username+"/repos", function( data ) {
       $('.result').append("&nbsp;&nbsp;&nbsp;&nbsp;<h3>Repos</h3>");
       for(var i=0; i < data.length; i++) {
-        console.log(data[i]);
-        // $(".result").append(data[i].name + "<br/>");
         $(".result").append("&nbsp;&nbsp;&nbsp;&nbsp;" + data[i].name + "<br/>");
-      }
-    });
-    $.get( "https://api.github.com/repos/devleague/gee-mail/commits", function( data ) {
-      $('.result').append("&nbsp;&nbsp;&nbsp;&nbsp;<h3>gee-mail commits</h3>");
-      for(var i=0; i < data.length; i++) {
-        console.log(data[i]);
-        // $(".result").append(data[i].name + "<br/>");
-        $(".result").append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + data[i].commit.message + "<br/>");
       }
     });
   };
 
-  var listDefaultRepo = function() {
-    $('.result').append("<h2>devleague</h2>");
+  var listDevLeagueRepos = function() {
     $.get( "https://api.github.com/orgs/devleague/repos", function( data ) {
+      $('.result').append("<h3>devleague repos</h3>");
       for(var i=0; i < data.length; i++) {
-        console.log(data[i].name);
-        // $(".result").append(data[i].name + "<br/>");
         $(".result").append("&nbsp;&nbsp;&nbsp;&nbsp;" + data[i].name + "<br/>");
+      }
+    });
+  };
+
+  var listGeemailCommits = function() {
+    $.get( "https://api.github.com/repos/devleague/gee-mail/commits", function( data ) {
+      $('.result').append("&nbsp;&nbsp;&nbsp;&nbsp;<h3>gee-mail commits</h3>");
+      for(var i=0; i < data.length; i++) {
+        $(".result").append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + data[i].commit.message + "<br/>");
+      }
+    });
+  };  
+
+  var listTheRemixCommits = function() {
+    $.get("https://api.github.com/repos/devleague/100-Specs/commits?author=theRemix", function( data ) {
+      $('.result').append("&nbsp;&nbsp;&nbsp;&nbsp;<h3>theRemix commits</h3>");
+      for(var i=0; i < data.length; i++) {
+        $('.result').append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + data[i].commit.message + "<br/>");
       }
     });
   };
